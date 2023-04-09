db3e8695342ff83c12a4536e0a72a7b37c0d1f0a
AmanHayat007 <111421111+AmanHayat007@users.noreply.github.com>
2023-04-09 17:44:00 +0530

    Number patterns

---
+++ cw6.py
@@ -0,0 +1,9 @@
+rows = int(input("Enter the number of rows: "))  
+k = 0  
+for i in range(rows, 0, -1):  
+    k += 1  
+    for j in range(0, rows-i):
+        print(end=" ")
+    for j in range(1, i + 1):  
+        print(k, end=' ')  
+    print()  
